50f9833a8dcbc76c8a5cbc6e38a8077cfd92c834
DeviceIoControl <joshsteph@hotmail.co.uk>
2021-12-05 21:13:24 +0000

    Added main to SvcManager.hpp

--- SvcManager.hpp
+++ SvcManager.hpp
@@ -190,6 +190,11 @@ private:
 // Run program: Ctrl + F5 or Debug > Start Without Debugging menu
 // Debug program: F5 or Debug > Start Debugging menu
 
+int main() 
+{
+	return 0;
+}
+
 // Tips for Getting Started: 
 //   1. Use the Solution Explorer window to add/manage files
 //   2. Use the Team Explorer window to connect to source control
