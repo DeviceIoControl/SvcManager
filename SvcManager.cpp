8f2be3901900caab93e7c9e7468430e272d0ca44
Josh S <39340430+DeviceIoControl@users.noreply.github.com>
2023-09-16 16:23:56 +0100

    fix: Fixed up comments and updated SDK version.

--- SvcManager.cpp
+++ SvcManager.cpp
@@ -1,4 +1,4 @@
-// SvcManager.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//  SvcManager.cpp : This file contains the 'main' function. Program execution begins and ends there.
 
 #include <Windows.h>
 #include <algorithm>
@@ -25,10 +25,10 @@ public:
 		std::swap(svcHandle.m_DispName, this->m_DispName);
 	}
 
-	//Start the service.
+	// Start the service.
 	bool Start(const std::vector<std::string>& args) const
 	{
-		//I can't think of a safer way to do this...
+		// I can't think of a safer way to do this...
 		std::unique_ptr<const char*> lpArgs(new const char* [args.size()]);
 		const char** lpszArgs = lpArgs.get();
 
@@ -42,7 +42,7 @@ public:
 
 	inline const std::string& Name() const { return this->m_DispName; }
 
-	//Check if the service handle is valid.
+	// Check if the service handle is valid.
 	inline bool Valid() const
 	{
 		return (this->m_SvcHandle != static_cast<SC_HANDLE>(INVALID_HANDLE_VALUE));
@@ -50,21 +50,21 @@ public:
 
 	inline operator SC_HANDLE() const { return this->m_SvcHandle; }
 
-	//Stop the service.
+	// Stop the service.
 	inline bool Stop() const
 	{
 		SERVICE_STATUS svcStatus = { 0 };
 
-		//Send a "SERVICE_CONTROL_STOP" message the service.
+		// Send a "SERVICE_CONTROL_STOP" message the service.
 		BOOL success = ControlService(this->m_SvcHandle, SERVICE_CONTROL_STOP, &svcStatus);
 
-		//Check if the message is supported otherwise, we just return false.
+		// Check if the message is supported otherwise, we just return false.
 		return (svcStatus.dwControlsAccepted & SERVICE_ACCEPT_STOP) ? success : false;
 	}
 
 	ServiceHandle& operator=(const ServiceHandle& rhs) = delete;
 
-	//Move assignment operator.
+	// Move assignment operator.
 	ServiceHandle& operator=(ServiceHandle&& rhs) noexcept
 	{
 		std::swap(this->m_SvcHandle, rhs.m_SvcHandle);
@@ -72,7 +72,7 @@ public:
 		return *this;
 	}
 
-	//NOTE: Only closes the service handle. This does NOT delete the service from the system.
+	// NOTE: Only closes the service handle. This does NOT delete the service from the system.
 	~ServiceHandle()
 	{
 		CloseServiceHandle(this->m_SvcHandle);
@@ -83,11 +83,11 @@ private:
 	std::string m_DispName;
 };
 
-//It will cause a declaration clash if I don't do this.
+// It will cause a declaration clash if I don't do this.
 #undef OpenService
 #undef CreateService
 
-//Service access constants.
+// Service access constants.
 enum class SVC_ACCESS : uint32_t
 {
 	STOP = SERVICE_STOP,
@@ -100,7 +100,7 @@ enum class SVC_ACCESS : uint32_t
 	USER_DEFINED_CONTROL = SERVICE_USER_DEFINED_CONTROL
 };
 
-//Windows Service Start Types.
+// Windows Service Start Types.
 enum class SVC_START_TYPE : uint32_t
 {
 	BOOT = SERVICE_BOOT_START,
@@ -110,7 +110,7 @@ enum class SVC_START_TYPE : uint32_t
 	DISABLED = SERVICE_DISABLED
 };
 
-//Windows Service Error Control params.
+// Windows Service Error Control params.
 enum class SVC_ERROR_CTRL : uint32_t
 {
 	ERROR_IGNORE = SERVICE_ERROR_IGNORE,
@@ -119,7 +119,7 @@ enum class SVC_ERROR_CTRL : uint32_t
 	ERROR_CRITICAL = SERVICE_ERROR_CRITICAL
 };
 
-//Windows Service Types.
+// Windows Service Types.
 enum class SVC_TYPE : uint32_t
 {
 	KERNEL_DRIVER = SERVICE_KERNEL_DRIVER,
@@ -139,7 +139,7 @@ public:
 		return (ServiceManager::m_SvcManager == INVALID_HANDLE_VALUE) ? false : true;
 	}
 
-	//Creates a user specified service.
+	// Creates a user specified service.
 	static ServiceHandle CreateService(const std::string& svcName, const std::string& svcDispName,
 		SVC_TYPE ServiceType, SVC_START_TYPE StartType, SVC_ERROR_CTRL ErrorControl,
 		const std::string& svcBinPath, const std::string& svcUserName = "",
@@ -172,13 +172,13 @@ public:
 		return ServiceHandle(svcHandle, "N/A");
 	}
 
-	//Deletes the service.
+	// Deletes the service.
 	inline static bool DeleteService(const ServiceHandle& svcHandle)
 	{
 		return ::DeleteService(svcHandle);
 	}
 
-	//Shutdown the Service Manager.
+	// Shutdown the Service Manager.
 	inline static bool Shutdown()
 	{
 		return CloseServiceHandle(ServiceManager::m_SvcManager);
@@ -190,7 +190,7 @@ private:
 
 int main(int argc, const char** argv)
 {
-	//------------------Service Management API-------------------------
+	// ------------------Service Management API-------------------------
 
 	ServiceManager::Initialize();
 
@@ -205,19 +205,8 @@ int main(int argc, const char** argv)
 	ServiceManager::DeleteService(svcHandle);
 	ServiceManager::Shutdown();
 
-	//-----------------------------------------------------------------
+	// -----------------------------------------------------------------
 
 	std::getchar();
 	return 0;
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
